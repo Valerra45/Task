9c98dfce3f7db42d99119a959786696f66fdb96a
Valerra45 <valerra45@gmail.com>
2022-06-14 22:00:07 +0300

    daa validators importance command

---
+++ src/Task.Api/Task.Api.Application/Services/Importances/Validators/UpdateImportanceValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tasks.Api.Application.Services.Importances.Commands;
+
+namespace Tasks.Api.Application.Services.Importances.Validators
+{
+    public class UpdateImportanceValidator : AbstractValidator<UpdateImportanceCommand>
+    {
+        public UpdateImportanceValidator()
+        {
+            RuleFor(x => x.Importance.Name)
+             .NotEmpty()
+             .WithMessage("{PropertyName} is not assigned");
+        }
+    }
+}
