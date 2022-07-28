049705719e1c6b652e756b4ca237944fb7a6e31b
Valerra45 <valerra45@gmail.com>
2022-07-28 23:34:20 +0300

    refactoring documenttask controller

---
+++ src/Task.Api/Task.Api.Application/Services/DocumentTasks/DocumentTaskByTimeRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks.Api.Application.Services.DocumentTasks
+{
+    public class DocumentTaskByTimeRequest
+    {
+        public DateTime? DateStart { get; set; }
+    }
+}
---
+++ src/Task.Api/Task.Api.Application/Services/DocumentTasks/DocumentTaskByUserNameRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks.Api.Application.Services.DocumentTasks
+{
+    public class DocumentTaskByUserNameRequest
+    {
+        public string? UserName { get; set; }
+    }
+}
